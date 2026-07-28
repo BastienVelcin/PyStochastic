8d62e770e32df0932c8fb44a35d8b2c63e7ab8ca
BuildTools <unconfigured@null.spigotmc.org>
2026-07-28 17:36:50 +0200

    - Added Beta, Weibull and Frechet sampling functions - Added density.py : file that lists all densities of crandom.py distributions.

---
+++ dist/density.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def dbeta(a,b,x):
+    return x**(a-1)*(1-x)**(b-1)
--- random/crandom.py
+++ random/crandom.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sys
 
+import scipy
+import scipy as sp
+
+from dist import density
+
+
 #######################################################################################################
 # CRANDOM MODULE                                                                                      #
 #                                                                                                     #
@@ -10,12 +16,12 @@ import sys
 # - Exponential                                                                                       #
 # - Normal                                                                                            #
 # - Gamma                                                                                             #
-#                                                                                                     #
-# TO DO :                                                                                             #
-#                                                                                                     #
 # - Beta                                                                                              #
 # - Weibull                                                                                           #
 # - Frechet                                                                                           #
+#                                                                                                     #
+# TO DO :                                                                                             #
+#                                                                                                     #
 # - Cauchy                                                                                            #
 # - Gumbel                                                                                            #
 # - Kumaraswamy                                                                                       #
@@ -43,6 +49,8 @@ def uniform(a=0,b=1,n=1):
     return u
 
 def exponential(alpha=1,n=1):
+    if alpha < 0:
+        raise ValueError("The parameter should be greater than 0.")
 
     '''
 
@@ -67,7 +75,7 @@ def normal(mean=0, sd=1,n=1):
     if sd <= 0:
         raise ValueError("The standard deviation should be greater than 0.")
 
-    # U and V should not be equal to 0 when we implement Box-Muller, because we can't compute log(0).
+    # U should not be equal to 0 when we implement Box-Muller, because we can't compute log(0).
     U = uniform(sys.float_info.epsilon,1,n)
     V = uniform(0,1,n)
 
@@ -92,4 +100,48 @@ def gamma(p=1, theta=1,n=1):
     E = exponential(1/theta,n)
     return np.array(sum(E))
 
-def beta(a=1,b=1,n=1)
+def beta(a=1,b=1,n=1):
+
+    '''
+
+    :param a: first shape parameter
+    :param b: second shape parameter
+    :param n: number of samples
+
+    The beta function returns n samples of a beta distribution of parameters (a,b), according to the following result :
+    X ~ Gamma(a,1), Y ~ Gamma(b,1) ==> X/(X+Y) ~ Beta(a,b)
+    '''
+
+    X = gamma(a,1,n)
+    Y = gamma(b,1,n)
+    return X/(X+Y)
+
+def weibull(k=1, l=1, n=1):
+
+    '''
+
+    :param k: shape parameter
+    :param l: scale parameter
+    :param n: number of samples
+
+    The weibull function returns n samples of a Weibull distribution of parameters (k,l).
+    '''
+
+    U = uniform(0,1,n)
+    return l*(-np.log(1-U))**(1/k)
+
+def frechet(a=1,s=1,m=0,n=1):
+
+    '''
+
+    :param a: shape parameter
+    :param s: scale parameter
+    :param m: position parameter
+    :param n: number of samples
+
+    The frechet function returns n samples of a Fréchet distribution of parameters (a,s,m).
+    '''
+
+    U = uniform(0, 1, n)
+    return m+s*(-np.log(U))**(-1/a)
+
