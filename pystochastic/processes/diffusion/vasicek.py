e895b50d007396965bc80fab1a6409959ef2cdf1
BuildTools <unconfigured@null.spigotmc.org>
2026-08-28 12:52:10 +0200

    Fixed density for Vasicek and OU

--- pystochastic/processes/diffusion/vasicek.py
+++ pystochastic/processes/diffusion/vasicek.py
@@ -367,5 +367,5 @@ class Vasicek(DiffusionProcess):
         if t == 0:
             return np.array([0])
         N = Normal(mu = np.exp(-self.speed*t)*(self.initial - self.mean) + self.mean,
-                   var = self.volatility/2*self.speed) * (1-np.exp(-2*t*self.speed))
+                   var = self.volatility**2/2*self.speed) * (1-np.exp(-2*t*self.speed))
         return N.pdf(x)
